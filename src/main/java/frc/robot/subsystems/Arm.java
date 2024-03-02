4f1a32398fe12c847a89db3feff1e75cd8ecf205
Robotics Team 28 <Piersonrobotics@sagharborschools.org>
2024-03-02 13:07:31 -0500

    Update new control mode for Arm motor

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
@@ -8,25 +10,39 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
     private TalonFX motor;
+    private MotionMagicVoltage motionMagicController = new MotionMagicVoltage(0);
     
 
     public Arm() {
         // TO DO: Perfect slot configs for motor control
         motor = new TalonFX(-4);
-        var slot0Configs = new Slot0Configs();
-        slot0Configs.kP = 24;
-        slot0Configs.kI = 0;
-        slot0Configs.kD = 0.1;
-        motor.getConfigurator().apply(slot0Configs);
+        var talonFXConfigs = new TalonFXConfiguration();
+        // set slot 0 gains
+        var slot0Configs = talonFXConfigs.Slot0;
+        slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
+        slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+        slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+        slot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
+        slot0Configs.kI = 0; // no output for integrated error
+        slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+
+        // set Motion Magic settings
+        var motionMagicConfigs = talonFXConfigs.MotionMagic;
+        motionMagicConfigs.MotionMagicCruiseVelocity = 80; // Target cruise velocity of 80 rps
+        motionMagicConfigs.MotionMagicAcceleration = 160; // Target acceleration of 160 rps/s (0.5 seconds)
+        motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
+
+        motor.getConfigurator().apply(talonFXConfigs);
         motor.setPosition(0);
     }
 
     public void moveToPose(double pose){
-        motor.setControl(new PositionVoltage(pose, .1, false, 0, 0, false, false, false));
+        motor.setControl(motionMagicController.withPosition(pose));
     }
 
     public void down(){
-        motor.setVoltage(2);
+        double newPosition = motor.getPosition().getValueAsDouble() + .1;
+        motor.setControl(motionMagicController.withPosition(newPosition));
     }
 
     public void up(){
